45c6a6e5246b0166b00dbe3d704b08abb6d1d058
serge <sergei.g.d@gmail.ccom>
2022-11-07 16:05:15 +0700

    4.3. готово.

--- src/main/resources/liquibase/scripts/lesson-three.sql
+++ src/main/resources/liquibase/scripts/lesson-three.sql
@@ -1,7 +1,7 @@
-- liquibase formatted sql
+-- liquibase formatted sql
 
-- changeset sk:1
+-- changeset sk:1
 CREATE INDEX student_n_idx ON student(name);
 
-- changeset sk:2
+-- changeset sk:2
 CREATE INDEX faculty_nc_idx ON faculty(name, color);
